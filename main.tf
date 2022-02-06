eea50d7d5b6947f19cf95fd4e8e43a76840765fd
Aleksey Podkidyshev <podkidyshev.as@phystech.edu>
2022-02-06 20:50:33 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,13 +7,13 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "<OAuth-token>" # get using yc iam access-key create --service-account-name service-account-for-cf 
+  token     = "<OAuth-token>" # get using docs: https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token
   cloud_id  = "<CLOUD-ID>" # your public service account key
   folder_id = "<FOLDER-ID>" # your secret service account key
 }
 
 resource "yandex_storage_bucket" "bucket" {
-  access_key = "<ACCESS-KEY>"
-  secret_key = "<SECRET-KEY>"
+  access_key = "<ACCESS-KEY>" # Use > yc iam access-key create --service-account-name service-account-for-cf
+  secret_key = "<SECRET-KEY>" # Use > yc iam access-key create --service-account-name service-account-for-cf
   bucket = "alekseyen-bucket-for-trigger" # your bucket name
 } 
